b8e8b77e08a75940905fb43ed6dc129c797acac2
Evan <evan.ruoss@gmail.com>
2018-06-29 16:30:42 -0500

    add var package for 07_variables

---
+++ 07_variables/01_shorthand/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/evanruoss/udemyTraining/07_variables/vars/"
+)
+
+func main() {
+
+	fmt.Println("My Name is", (vars.MyName))
+
+}
---
+++ 07_variables/vars/name.go
@@ -0,0 +1,4 @@
+package vars
+
+var MyName = "Evan"
+var Age = "28"
