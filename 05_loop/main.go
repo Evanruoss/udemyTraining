6802f08bf669e0f7cbd2be48a58faee19e93c321
Evan <evan.ruoss@gmail.com>
2018-06-29 15:13:06 -0500

    minor change

--- 05_loop/main.go
+++ 05_loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 10000000; i < 100000000; i++ {
-		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	for i := 10; i < 100; i++ {
+		fmt.Printf("%d \t %b \t %#X \n", i, i, i)
 	}
 }
